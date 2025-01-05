68d7041809136cbd87fd38b7dac6f85eba0ad81a
Sonali Sharma <sonalisharma2832@gmail.com>
2025-01-05 06:26:29 -0500

    task activities

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import conn from "../backend/conn/conn";
 const app = express();
-const auth = 
+const auth = require("../models/auth");
+const list = require("../models/list"); 
 app.use(express.json());
 
 app.use("/api/v1", auth);
+app.use("/api/v2", list);
+
 app.listen(1000, ()=> {
     console.log("Server Started");
 })
